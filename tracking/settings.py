7964c7f2639e8792fbc234df0160645a02c8108a
Dauda-yusuf <44580547+Dauda-yusuf@users.noreply.github.com>
2022-09-13 04:25:20 -0700

    Update settings.py

--- tracking/settings.py
+++ tracking/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-($+#00z!sa(skf%jwzm@ra66qt%pgqxdsg+57clm^v+sf2la9d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
